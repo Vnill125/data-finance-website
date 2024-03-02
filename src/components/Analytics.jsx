b905efc3c9ad839af7ddff1c1324bc9afbbc8d1b
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2024-03-02 19:43:37 +0100

    Update Analytics.jsx

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -10,12 +10,14 @@ const Analytics = () => {
       <div className='flex flex-col justify-center p-4'>
         <p className='font-bold text-[#00df9a] text-xl py-2'>DATA ANALYTICS DASHBOARD</p>
         <h1 className='font-bold text-black text-3xl'>Manage Data Analytics Centrally</h1>
+        
         <p className='text-black text-lg'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
           molestiae delectus culpa hic assumenda, voluptate reprehenderit
           dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
           eveniet ex deserunt fuga?
         </p>
+        
         <button className='bg-black w-[200px] mx-auto rounded-md text-[#00df9a] font-medium py-3 hover:opacity-60 hover:py-4
          hover:w-[220px] duration-300 hover:text-lg mt-14'>Get Started</button>
       </div>
