9139e32d5f91d4c289529b2eaa2f03750e51ca32
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2024-03-14 17:29:43 +0100

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { useLocation } from "react-router-dom";
 import { TfiAngleRight } from "react-icons/tfi";
 import { TfiAlignJustify } from "react-icons/tfi";
 import { useState } from 'react'
@@ -12,16 +13,45 @@ const NavBar = () => {
       setNav(!nav)
     }
 
+    const pathName = useLocation();
+
+    const Navigation = [
+      {
+        id: "1",
+        title: "Home",
+        url: "/Home",
+      },
+      {
+        id: "2",
+        title: "Company",
+        url: "/Company",
+      },
+      {
+        id: "3",
+        title: "Resources",
+        url: "/Resources",
+      },
+      {
+        id: "4",
+        title: "About",
+        url: "/About",
+      },
+      {
+        id: "5",
+        title: "Contact",
+        url: "/Contact",
+      },
+    ]
+
   return (
     <div className='text-white max-w-[1240px] mx-auto h-24 flex px-4 justify-between items-center'>
       <h1 className="text-[#00df9a] text-3xl w-full font-bold">REACT.</h1>
       <ul className='flex max-lg:hidden'>
-        
-        <li className='p-4 uppercase hover:opacity-60 font-semibold cursor-pointer hover:border-b-4 duration-75 border-b-[#00df9a]'>Home</li>
-        <li className='p-4 uppercase hover:opacity-60 font-semibold cursor-pointer hover:border-b-4 duration-75 border-b-[#00df9a]'>Company</li>
-        <li className='p-4 uppercase hover:opacity-60 font-semibold cursor-pointer hover:border-b-4 duration-75 border-b-[#00df9a]'>Resources</li>
-        <li className='p-4 uppercase hover:opacity-60 font-semibold cursor-pointer hover:border-b-4 duration-75 border-b-[#00df9a]'>About</li>
-        <li className='p-4 uppercase hover:opacity-60 font-semibold cursor-pointer hover:border-b-4 duration-75 border-b-[#00df9a]'>Contact</li>
+        {Navigation.map((item) => (
+          <a key={item.id} href={item.url}><li className={`p-4 uppercase hover:opacity-60 font-semibold
+           cursor-pointer hover:border-b-4 duration-75 border-b-[#00df9a] ${item.url == pathName.pathname ? ' text-[#00df9a]' : 'text-white'}`}>{item.title}</li></a>
+        ))}
+        {console.log(pathName.hash)}
       </ul>
 
       <div onClick={handleNav} className='lg:hidden'>
@@ -30,11 +60,9 @@ const NavBar = () => {
       
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]' : 'fixed left-[-100%]'}>
         <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600 hover:opacity-60 cursor-pointer'>Home</li>
-          <li className='p-4 border-b border-gray-600 hover:opacity-60 cursor-pointer'>Company</li>
-          <li className='p-4 border-b border-gray-600 hover:opacity-60 cursor-pointer'>Resources</li>
-          <li className='p-4 border-b border-gray-600 hover:opacity-60 cursor-pointer'>About</li>
-          <li className='p-4 hover:opacity-60 cursor-pointer'>Contact</li>
+          {Navigation.map((item) => (
+            <a key={item.id} href={item.url}><li className='p-4 border-b border-gray-600 hover:opacity-60 cursor-pointer'>{item.title}</li></a>
+          ))}
       </ul>
     </div>
   )
