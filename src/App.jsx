3b21d985c292b67b6dce72127a04346f75553618
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2024-03-14 17:29:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter } from "react-router-dom"
 
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
@@ -9,13 +10,14 @@ import Newsletter from './components/Newsletter'
 const App = () => {
   return (
     <div>
-      <NavBar/>
-      <Hero/>
-      <Analytics/>
-      <Newsletter/>
+      <BrowserRouter>
+        <NavBar/>
+        <Hero/>
+        <Analytics/>
+        <Newsletter/>
+      </BrowserRouter>
     </div>
   )
 }
 
 export default App
-
