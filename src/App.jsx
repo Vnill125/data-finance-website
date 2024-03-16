cc4e89c78d12af79a28dd538b8f51a2a11a7957e
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2024-03-16 13:55:47 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Hero from './components/Hero'
 import Analytics from './components/Analytics'
 import Newsletter from './components/Newsletter'
 import Cards from './components/Cards'
+import Footer from './components/Footer'
+
 
 
 const App = () => {
@@ -17,6 +19,7 @@ const App = () => {
         <Analytics/>
         <Newsletter/>
         <Cards/>
+        <Footer/>
       </BrowserRouter>
     </div>
   )
