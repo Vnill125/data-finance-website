06e73ffba028e0904454ce9870e9f5b50567d8ab
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2024-03-16 13:54:42 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,20 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
   return (
-    <div className='mt-[-80px] h-screen flex justify-center flex-col text-center text-white'>
-      <p className='text-[#00df9a] font-bold text-[20px] md:text-[25px] lg:text-[30px] uppercase'>Growing with data analytics</p>
-      <h1 className='text-[25px] md:text-[30px] lg:text-[35px] pb-8 font-bold uppercase'>Grow with data.</h1>
-      <div className='flex justify-center'>
-        <p className='text-xl md:text-2xl lg:text-4xl pb-2 font-bold mx-2'>Flex, flexible financing for</p>
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold p-2'>
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
+          Grow with data.
+        </h1>
+        <div className='flex justify-center items-center'>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
+            Fast, flexible financing for
+          </p>
         <TypeAnimation 
-        className='text-xl md:text-2xl text-gray-400 lg:text-4xl font-bold e'
+        className='md:text-5xl sm:text-4xl text-xl text-gray-500 font-bold md:pl-4 pl-2'
         sequence={[
           'BTB', 
           1500,
@@ -20,10 +27,10 @@ const Hero = () => {
         ]}
         speed={10}
         repeat={Infinity} />
+        </div>
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+        <button className='bg-[#00df9a] w-[200px] mx-auto rounded-md text-black font-semibold mt-10 py-3 hover:opacity-60 hover:scale-[1.15] duration-300 hover:text-lg'>Get Started</button>
       </div>
-      <p className='md:text-xl lg:text-2xl pb-8 font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
-        <button className='bg-[#00df9a] w-[200px] mx-auto rounded-md text-black font-semibold mt-6 py-3 hover:opacity-60 hover:py-4
-         hover:w-[220px] duration-300 hover:text-lg'>Get Started</button>
     </div>
   )
 }
